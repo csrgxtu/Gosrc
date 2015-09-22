ddf5eb796349b715312f851cc8e756a21777096f
archer <1246506786@qq.com>
2015-09-22 04:43:30 -0400

    go tool go path and the project structure

--- src/github.com/csrgxtu/hello/hello.go
+++ src/github.com/csrgxtu/hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/csrgxtu/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
