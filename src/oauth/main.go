aa6ac04a47f5ed05d0830607e0c4ca7d4a2cf9a9
archer <1246506786@qq.com>
2015-10-26 02:49:10 -0400

    github oauth worked 2

--- src/oauth/main.go
+++ src/oauth/main.go
@@ -15,12 +15,19 @@ func main() {
 
   m.Use(render.Renderer())
   m.Use(sessions.Sessions("my_session", sessions.NewCookieStore([]byte("secret123"))))
-  m.Use(oauth2.Google(
+  // m.Use(oauth2.Google(
+  //   &goauth2.Config{
+  //     ClientID:     "393837115699-selpvjkb9bl6orqar3915dpb142u5u27.apps.googleusercontent.com",
+  //     ClientSecret: "CQm04gHSRATBsZrXVqVXmcj6",
+  //     Scopes:       []string{"https://www.googleapis.com/auth/drive"},
+  //     RedirectURL:  "http://localhost:3000/apis",
+  //   },
+  // ))
+  m.Use(oauth2.Github(
     &goauth2.Config{
-      ClientID:     "393837115699-selpvjkb9bl6orqar3915dpb142u5u27.apps.googleusercontent.com",
-      ClientSecret: "CQm04gHSRATBsZrXVqVXmcj6",
-      Scopes:       []string{"https://www.googleapis.com/auth/drive"},
-      RedirectURL:  "http://localhost:3000/apis",
+      ClientID: "855acacb3bfe44dd929b",
+      ClientSecret: "da676b7e863cf493f9fcac0eb856cbb2074c5806",
+      RedirectURL: "http://localhost:3000/apis",
     },
   ))
 
