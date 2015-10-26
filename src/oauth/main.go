d6569c11d8a528a282987609ac3fb208c2ac1cdb
archer <1246506786@qq.com>
2015-10-26 03:21:36 -0400

    commit for demorize to cto, but not ready yet

--- src/oauth/main.go
+++ src/oauth/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-  "fmt"
+	//"fmt"
 
-  "github.com/go-martini/martini"
-  "github.com/martini-contrib/render"
-  "github.com/martini-contrib/sessions"
-  "github.com/martini-contrib/oauth2"
-  goauth2 "golang.org/x/oauth2"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/oauth2"
+	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/sessions"
+	goauth2 "golang.org/x/oauth2"
 
-  "oauth/controllers"
+	"oauth/controllers"
 )
 
 func main() {
-  m := martini.Classic()
+	m := martini.Classic()
 
-  m.Use(render.Renderer())
-  m.Use(sessions.Sessions("my_session", sessions.NewCookieStore([]byte("secret123"))))
-  // m.Use(oauth2.Google(
-  //   &goauth2.Config{
-  //     ClientID:     "393837115699-selpvjkb9bl6orqar3915dpb142u5u27.apps.googleusercontent.com",
-  //     ClientSecret: "CQm04gHSRATBsZrXVqVXmcj6",
-  //     Scopes:       []string{"https://www.googleapis.com/auth/drive"},
-  //     RedirectURL:  "http://localhost:3000/apis",
-  //   },
-  // ))
-  m.Use(oauth2.Github(
-    &goauth2.Config{
-      ClientID: "855acacb3bfe44dd929b",
-      ClientSecret: "da676b7e863cf493f9fcac0eb856cbb2074c5806",
-      RedirectURL: "http://localhost:3000/apis",
-    },
-  ))
+	m.Use(render.Renderer())
+	m.Use(sessions.Sessions("my_session", sessions.NewCookieStore([]byte("secret123"))))
+	// m.Use(oauth2.Google(
+	//   &goauth2.Config{
+	//     ClientID:     "393837115699-selpvjkb9bl6orqar3915dpb142u5u27.apps.googleusercontent.com",
+	//     ClientSecret: "CQm04gHSRATBsZrXVqVXmcj6",
+	//     Scopes:       []string{"https://www.googleapis.com/auth/drive"},
+	//     RedirectURL:  "http://localhost:3000/apis",
+	//   },
+	// ))
+	m.Use(oauth2.Github(
+		&goauth2.Config{
+			ClientID:     "855acacb3bfe44dd929b",
+			ClientSecret: "da676b7e863cf493f9fcac0eb856cbb2074c5806",
+			RedirectURL:  "http://localhost:3000/apis",
+		},
+	))
 
-  m.Get("/apis", controllers.Create)
-  m.Get("/", controllers.Welcome)
-  m.Get("/auth", oauth2.LoginRequired, func(tokens oauth2.Tokens) string {
-    return tokens.Access()
-  })
+	m.Get("/apis", controllers.Create)
+	m.Get("/", controllers.Welcome)
+	m.Get("/auth", oauth2.LoginRequired, func(tokens oauth2.Tokens) string {
+		return tokens.Access()
+	})
 
-  m.Run()
+	m.Run()
 }
